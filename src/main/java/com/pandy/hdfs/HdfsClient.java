13496a110db9508ee716a61f7e7df970b77e9e41
PandyYang <yangpandy@gmail.com>
2022-07-17 19:54:42 +0800

    file operates

--- src/main/java/com/pandy/hdfs/HdfsClient.java
+++ src/main/java/com/pandy/hdfs/HdfsClient.java
@@ -1,8 +1,7 @@
 package com.pandy.hdfs;
 
 import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.fs.FileSystem;
-import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.fs.*;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -10,6 +9,7 @@ import org.junit.Test;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.Arrays;
 
 /**
  * 客户端代码
@@ -38,6 +38,7 @@ public class HdfsClient {
 
     }
 
+    // 创建文件夹
     @Test
     public void mkdir() throws IOException {
 
@@ -68,6 +69,7 @@ public class HdfsClient {
                 new Path("/xiyou/huaguoshan"));
     }
 
+    // 下载文件
     @Test
     public void getFile() throws IOException {
         // 是否删除源文件 删除hdfs上的源文件
@@ -79,5 +81,64 @@ public class HdfsClient {
                 new Path("/xiyou/huaguoshan/log4j.properties"),
                 new Path("C:\\Users\\Administrator\\IdeaProjects\\HDFS\\src\\main\\resources\\log4j.properties2"),
                 true);
+    }
+
+    // 移动文件 和 重命名
+    @Test
+    public void moveFile() throws IOException {
+        // src源文件路径
+        // dest 目标文件路径
+        fileSystem.rename(
+                new Path("/xiyou/huaguoshan/log4j.properties"),
+                new Path("/xiyou/huaguoshan/log4j.properties2"));
+    }
+
+    @Test
+    public void moveFile2() throws IOException {
+        // 修改至不同目录下的不同名称
+        fileSystem.rename(
+                new Path("/xiyou/huaguoshan/log4j.properties2"),
+                new Path("/log4j.properties")
+        );
+    }
+
+    @Test
+    public void moveDir() throws IOException {
+
+        // 目录更名
+        fileSystem.rename(
+                new Path("/input"),
+                new Path("/input2")
+        );
+    }
+
+    @Test
+    public void fileDetails() throws IOException {
+        RemoteIterator<LocatedFileStatus> files = fileSystem.listFiles(new Path("/"), true);
+        while (files.hasNext()) {
+            System.out.println("======================================");
+            LocatedFileStatus fileStatus = files.next();
+            Path path = fileStatus.getPath();
+            System.out.println("name = " + path.getName());
+            String owner = fileStatus.getOwner();
+            System.out.println("owner = " + owner);
+            long len = fileStatus.getLen();
+            System.out.println("len = " + len);
+            long modificationTime = fileStatus.getModificationTime();
+            System.out.println("modificationTime = " + modificationTime);
+            long blockSize = fileStatus.getBlockSize();
+            System.out.println("blockSize = " + blockSize);
+
+
+            /**
+             * 块信息
+             * 大文件会分多个不同的块 一个块的大小是128M
+             * [0,134217728,hadoop102,hadoop103,hadoop104, 134217728,60795424,hadoop102,hadoop103,hadoop104]
+             */
+            BlockLocation[] blockLocations = fileStatus.getBlockLocations();
+            System.out.println("Arrays.toString(blockLocations) = " + Arrays.toString(blockLocations));
+
+            System.out.println("======================================");
+        }
     }
 }
